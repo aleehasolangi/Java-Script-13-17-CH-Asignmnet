8771210534750b28f650ed06db9ee6a1a8c46b3e
aleehasolangi <aleehasolangi@gmail.com>
2025-05-29 21:32:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,140 @@
+//Assignment 13-17 chapter
+// Arrays
+
+//1.
+
+// var students = [];
+
+
+//2.
+
+// var studentNames2 = new Array();
+
+
+//3.
+
+// var names = ["Aleeha", "Ali", "HEY"];
+
+
+//4.
+
+// var numbers = [1, 2, 3, 4, 5];
+
+
+//5.
+
+// var boolean = [true, false];
+
+
+//6.
+
+// var mix = ["Aleeha", 25, true, 5];
+
+
+//7.
+
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+// console.log(1 + ") " + qualifications[0]);
+// console.log(2 + ") " + qualifications[1]);
+// console.log(3 + ") " + qualifications[2]);
+// console.log(4 + ") " + qualifications[3]);
+// console.log(5 + ") " + qualifications[4]);
+// console.log(6 + ") " + qualifications[5]);
+// console.log(7 + ") " + qualifications[6]);
+// console.log(8 + ") " + qualifications[7]);
+
+
+//8.
+
+// var students = ["Michael", "John", "Tony"];
+// var scores = [400, 450, 300];
+// var total = 500;
+// console.log("Score of " + students[0] + " is " + scores[0] + ". Percentage is " + (scores[0] / total * 100) + "%");
+// console.log("Score of " + students[1] + " is " + scores[1] + ". Percentage is " + (scores[1] / total * 100) + "%");
+// console.log("Score of " + students[2] + " is " + scores[2] + ". Percentage is " + (scores[2] / total * 100) + "%");
+
+
+//9.
+
+
+// var colors = ["Red", "Green", "Blue", "Yellow"];
+// console.log(colors);
+
+//a.
+
+// colors.unshift("Black");
+// console.log(colors);
+
+//b.
+
+// colors.push("White");
+// console.log(colors);
+
+//c. 
+
+// colors.unshift("Pink", "Cyan");
+// console.log(colors);
+
+//d. 
+
+// colors.shift();
+// console.log(colors);
+
+//e.
+
+// colors.pop();
+// console.log(colors);
+
+//f. 
+
+// colors.splice(2, 0, "Gold");
+// console.log(colors);
+
+//g. 
+
+// colors.splice(1, 2);
+// console.log(colors);
+
+
+//10.
+
+// var scores = [320, 230, 480, 120];
+// scores.sort();
+// console.log("Sorted scores:", scores);
+
+
+//11.
+
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// var selectedcities = [];
+// selectedcities[0] = cities[2];
+// selectedcities[1] = cities[3];
+// console.log("All Cities: " + cities);
+// console.log("Selected Cities: " + selectedcities);
+
+
+//12.
+
+// var arr = ["This", " is", " my", " cat"];
+// console.log("Array:");
+// console.log(arr);
+// var st = arr.join("");
+// console.log("String:");
+// console.log(st);
+
+//15.
+
+var phones = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+document.write("Phone Brands:\n");
+document.write(phones[0] + "<br>");
+document.write(phones[1] + "<br>");
+document.write(phones[2] + "<br>");
+document.write(phones[3] + "<br>");
+document.write(phones[4] + "<br>");
+document.write(phones[5] + "<br>");
+
+
+
+
